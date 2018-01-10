62f25c392dfd245b0464d961afe190b0b35c484f
unknown <titouan.poisson@student.ecp.fr>
2018-01-10 20:58:38 +0100

    add PCA script

--- Correlation/Correlation selection.py
+++ Correlation/Correlation selection.py
@@ -28,19 +28,21 @@ def return_name(df,code):
     name = df.loc[df["Code"] == code]['Indicator Name'].values[0]
     return name
 
-def script_correlation(df,code_names,threshold):
+def script_correlation(df,code_names,threshold): #df = dataframe, #code_names = df with 2 columns code and names, #threshold = correlation threshold
+    """"Script to delete variables if they are too correlated """
 
     bool = True
-    while bool == True:
+    while bool:
+
         corr_mat = correlation_matrix(df)
         print(len(list(corr_mat)))
         delete = []
         stop = False
-        for i, row in enumerate(corr_mat.values):
+        for i, row in enumerate(corr_mat.values): #browsing correlation matrix
             # print("i" + str(i))
             for j in range(0, i):
                 # print("j" + str(j))
-                if row[j] > threshold:
+                if row[j] > threshold: #if correlation coeff is above threshold
                     delete = [i, j]
                     print(delete)
                     stop = True
@@ -50,20 +52,27 @@ def script_correlation(df,code_names,threshold):
         if not stop:  # No value below threshold was found
             bool = False
         else:
-            col_del_code = (list(corr_mat)[delete[0]], list(corr_mat)[delete[1]])
 
+            #Codes of the two variable that are compared
+            col_del_code = (list(corr_mat)[delete[0]], list(corr_mat)[delete[1]])
+            #Names of the two variable that are compared
             col_del_name = (return_name(code_names, col_del_code[0]), return_name(code_names, col_del_code[1]))
+            #Number of missing values of the two variable that are compared
             col_del_nan = (df[col_del_code[0]].isnull().sum(),df[col_del_code[1]].isnull().sum())
+
+            #Printing informations
             print( str(col_del_name[0]) + ": " + str(col_del_nan[0]) + " NaN" + ", " + str(col_del_name[1]) + ": " + str(col_del_nan[1]) + " NaN")
+
+            #Let user choose which variable to delete
             a = input("Quelle colonne supprimer ? (1/2)")
             if a == "1":
                 df = df.drop(columns=[col_del_code[0]])
             if a == "2":
                 df = df.drop(columns=[col_del_code[1]])
             else:
-                "Wrong input"
+                print("Wrong input")
                 pass
-
+    #Exporting final dataset
     df.to_csv("C:/Users/titou/Desktop/Centrale/Option OSY/Projet/Datasets/new_df.csv", sep=',')
 
 
